37cfb7d10d6c990bad38a38fe94c58b32dc21054
3ramayana <3ramayana@gmail.com>
2023-06-25 09:06:44 +0900

    add about page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -6,43 +6,32 @@ const About = () => {
     <>
       <Navbar />
 
-      <section className="bg-white dark:bg-gray-900 bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern.svg')] dark:bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern-dark.svg')]">
-        <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 z-10 relative">
-          <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-            Search, Apply & Get Your{' '}
-            <span className="text-blue-700">Dream Job</span>
-          </h1>
-          <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-200">
-            Start your hunt for the best, life-changing career opportunities
-            from here in your selected areas conveniently and get hired quickly
-          </p>
-          <form className="w-full max-w-md mx-auto">
-            <div className="relative">
-              <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                <svg
-                  className="w-5 h-5 text-gray-500 dark:text-gray-400"
-                  fill="currentColor"
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="1em"
-                  viewBox="0 0 512 512">
-                  <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
-                </svg>
-              </div>
-              <input
-                type="text"
-                className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:placeholder-gray-400 dark:text-white transition duration-300"
-                placeholder="Search jobs.."
-                required
+      <div className="py-16 max-w-screen-xl mx-auto">
+        <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+          <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
+            <div className="md:5/12 lg:w-5/12 rounded-xl overflow-hidden">
+              <img
+                src="https://tailus.io/sources/blocks/left-image/preview/images/startup.png"
+                alt=""
+                loading="lazy"
               />
-              <button
-                type="submit"
-                className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                Search
-              </button>
             </div>
-          </form>
+            <div className="md:7/12 lg:w-6/12">
+              <h2 className="text-2xl text-gray-900 dark:text-white font-bold md:text-4xl">
+                Make a best move to choose your best job
+              </h2>
+              <p className="mt-6 text-gray-600 dark:text-white">
+                If you looking for the perfect job, look no further. Presenting
+                DreamJob, your one-stop shop for all your job hunting needs.
+                From bespoke recomendations to "job of the day" notifications
+                and on to career counseliing, DreamJob has your job search
+                covered. As the no. 1 job search website, we built DreamJob to
+                work for you while you plan your move to your dream job.
+              </p>
+            </div>
+          </div>
         </div>
-      </section>
+      </div>
 
       <Footer />
     </>
